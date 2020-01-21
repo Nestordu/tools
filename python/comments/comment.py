12e45d17eba60130b4a8113a32075aa9cd910473
nestordu <nestor@sqstudio.com>
2020-01-21 13:04:40 +0800

    Comment: modify path

--- python/comments/comment.py
+++ python/comments/comment.py
@@ -4,18 +4,18 @@
 import json
 
 
-# class Comment(object):
-#     def __init__(self, name, title, body, rating, date, country):
-#         self.name = name
-#         self.title = title
-#         self.body = body
-#         self.rating = rating
-#         self.date = date
-#         self.country = country
-#
-#     def print_msg(self):
-#         print("%s %s" % (self.name, self.title))
-#
-#     def toJSON(self):
-#         return json.dumps(self, default=lambda o: o.__dict__,
-#                           sort_keys=True, indent=4)
+class Comment(object):
+    def __init__(self, name, title, body, rating, date, country):
+        self.name = name
+        self.title = title
+        self.body = body
+        self.rating = rating
+        self.date = date
+        self.country = country
+
+    def print_msg(self):
+        print("%s %s" % (self.name, self.title))
+
+    def toJSON(self):
+        return json.dumps(self, default=lambda o: o.__dict__,
+                          sort_keys=True, indent=4)
--- python/comments/get_app_comments.py
+++ python/comments/get_app_comments.py
@@ -5,7 +5,6 @@ import json
 from urllib import request
 from datetime import datetime
 import xlsxwriter
-# from .comment import Comment
 import math
 import time
 import random
@@ -13,140 +12,132 @@ import os
 import sys
 
 
-class Comment(object):
-    def __init__(self, name, title, body, rating, date, country):
-        self.name = name
-        self.title = title
-        self.body = body
-        self.rating = rating
-        self.date = date
-        self.country = country
+class GetComments(object):
 
-    def print_msg(self):
-        print("%s %s" % (self.name, self.title))
+    def get_comments(self, app_id, country, app_file):
+        from comments.comment import Comment
+        file_name = './comments/app_comments_%s.xlsx' % app_file
+        workbook = xlsxwriter.Workbook(file_name)
+        worksheet = workbook.add_worksheet('comments')
+        sheet_format = workbook.add_format()
+        sheet_format.set_border(1)
+        sheet_format.set_border(1)
+        format_title = workbook.add_format()
+        format_title.set_border(1)
+        format_title.set_bg_color('#cccccc')
+        format_title.set_align('left')
+        format_title.set_bold()
+        title = ['评分', '昵称', '评论内容', '时间', '国家']
+        worksheet.write_row('A1', title, format_title)
 
+        all_comments = []
 
-def get_comments(app_id, country, app):
+        count = 0
+        page = 50
+        for cc in country:
+            total = country[cc]
+            page_count = math.ceil(country[cc] / page)
+            for index in range(page_count):
+                start = index * page
+                end = (index + 1) * page
+                if end > total:
+                    end = total
 
-    file_name = 'app_comments_%s.xlsx' % app
-    workbook = xlsxwriter.Workbook(file_name)
-    worksheet = workbook.add_worksheet('comments')
-    sheet_format = workbook.add_format()
-    sheet_format.set_border(1)
-    sheet_format.set_border(1)
-    format_title = workbook.add_format()
-    format_title.set_border(1)
-    format_title.set_bg_color('#cccccc')
-    format_title.set_align('left')
-    format_title.set_bold()
-    title = ['评分', '昵称', '评论内容',  '时间', '国家']
-    worksheet.write_row('A1', title, format_title)
+                url = 'https://itunes.apple.com/WebObjects/MZStore.woa/wa/userReviewsRow?cc=%s&id=%s&displayable-kind=11' \
+                      '&startIndex=%s&endIndex=%s&sort=0&appVersion=all' % (cc, app_id, start, end)
 
-    all_comments = []
+                print('[%s_get_comments] country: %s start : %s end: %s  load ...' % (app_file, cc, start, end))
 
-    count = 0
-    page = 50
-    for cc in country:
-        total = country[cc]
-        page_count = math.ceil(country[cc]/page)
-        for index in range(page_count):
-            start = index * page
-            end = (index + 1) * page
-            if end > total:
-                end = total
+                req = request.Request(
+                    url,
+                    data=None,
+                    headers={
+                        'User-Agent': 'iTunes/11.0 (Windows; Microsoft Windows 7 Business Edition Service Pack 1 (Build '
+                                      '7601)) AppleWebKit/536.27.1 '
+                    }
+                )
 
-            url = 'https://itunes.apple.com/WebObjects/MZStore.woa/wa/userReviewsRow?cc=%s&id=%s&displayable-kind=11' \
-                  '&startIndex=%s&endIndex=%s&sort=0&appVersion=all' % (cc, app_id, start, end)
+                # load: {'userReviewList': [
+                #     {'userReviewId': '5340579214', 'body': 'Everything&#39;s good! Outfits are perfect, but too much ads!!!',
+                #      'date': '2019-12-31T00:24:34Z', 'name': 'raveilei', 'rating': 4, 'title': 'Too much ads!', 'voteCount': 0,
+                #      'voteSum': 0, 'isEdited': False,
+                #      'viewUsersUserReviewsUrl': 'https://itunes.apple.com/cn/reviews?userProfileId=477539462',
+                #      'voteUrl': 'https://userpub.itunes.apple.com/WebObjects/MZUserPublishing.woa/wa/rateUserReview?userReviewId=5340579214',
+                #      'reportConcernUrl': 'https://userpub.itunes.apple.com/WebObjects/MZUserPublishing.woa/wa/reportAConcernSubmit?cc=cn',
+                #      'reportConcernExplanation': '请提供关于此篇“超级造型师”评论的详细信息。评论作者不会看到您的报告。', 'customerType': 'Customers',
+                #      'reportConcernReasons': [{'reasonId': '0', 'name': '选取一个'},
+                #                               {'reasonId': '1', 'name': '此评论含有令人反感的内容', 'upperCaseName': '此评论含有令人反感的内容'},
+                #                               {'reasonId': '8', 'name': '此评论偏离主题', 'upperCaseName': '此评论偏离主题'},
+                #                               {'reasonId': '111003', 'name': '疑似垃圾内容', 'upperCaseName': '疑似垃圾内容'},
+                #                               {'reasonId': '7', 'name': '其他原因', 'upperCaseName': '其他原因'}]},
+                #     ]}
 
-            print('[%s_get_comments] country: %s start : %s end: %s  load ...' % (app, cc, start, end))
+                response = request.urlopen(req)
+                json_data = json.loads(response.read().decode())
 
-            req = request.Request(
-                url,
-                data=None,
-                headers={
-                    'User-Agent': 'iTunes/11.0 (Windows; Microsoft Windows 7 Business Edition Service Pack 1 (Build '
-                                  '7601)) AppleWebKit/536.27.1 '
-                }
-            )
+                # print('[get_comments]data: %s' % json_data)
 
-            # load: {'userReviewList': [
-            #     {'userReviewId': '5340579214', 'body': 'Everything&#39;s good! Outfits are perfect, but too much ads!!!',
-            #      'date': '2019-12-31T00:24:34Z', 'name': 'raveilei', 'rating': 4, 'title': 'Too much ads!', 'voteCount': 0,
-            #      'voteSum': 0, 'isEdited': False,
-            #      'viewUsersUserReviewsUrl': 'https://itunes.apple.com/cn/reviews?userProfileId=477539462',
-            #      'voteUrl': 'https://userpub.itunes.apple.com/WebObjects/MZUserPublishing.woa/wa/rateUserReview?userReviewId=5340579214',
-            #      'reportConcernUrl': 'https://userpub.itunes.apple.com/WebObjects/MZUserPublishing.woa/wa/reportAConcernSubmit?cc=cn',
-            #      'reportConcernExplanation': '请提供关于此篇“超级造型师”评论的详细信息。评论作者不会看到您的报告。', 'customerType': 'Customers',
-            #      'reportConcernReasons': [{'reasonId': '0', 'name': '选取一个'},
-            #                               {'reasonId': '1', 'name': '此评论含有令人反感的内容', 'upperCaseName': '此评论含有令人反感的内容'},
-            #                               {'reasonId': '8', 'name': '此评论偏离主题', 'upperCaseName': '此评论偏离主题'},
-            #                               {'reasonId': '111003', 'name': '疑似垃圾内容', 'upperCaseName': '疑似垃圾内容'},
-            #                               {'reasonId': '7', 'name': '其他原因', 'upperCaseName': '其他原因'}]},
-            #     ]}
+                cur_dict = {}
 
-            response = request.urlopen(req)
-            json_data = json.loads(response.read().decode())
+                if 'userReviewList' in json_data:
+                    count += len(json_data['userReviewList'])
+                    for item in json_data['userReviewList']:
+                        date = datetime.strptime(item['date'], '%Y-%m-%dT%H:%M:%SZ')
+                        comment = Comment(item['name'], item['title'], item['body'], item['rating'], date, cc)
+                        all_comments.append(comment)
+                        cur_dict[item['userReviewId']] = comment
+                else:
+                    print("")
+                    break
 
-            # print('[get_comments]data: %s' % json_data)
+                sleep_time = random.randint(1, 3)
+                time.sleep(sleep_time)
 
-            cur_dict = {}
+        all_comments.sort(key=lambda cm: cm.date, reverse=True)
 
-            if 'userReviewList' in json_data:
-                count += len(json_data['userReviewList'])
-                for item in json_data['userReviewList']:
-                    date = datetime.strptime(item['date'], '%Y-%m-%dT%H:%M:%SZ')
-                    comment = Comment(item['name'], item['title'], item['body'], item['rating'], date, cc)
-                    all_comments.append(comment)
-                    cur_dict[item['userReviewId']] = comment
-            else:
-                print("")
-                break
+        row = 1
+        col = 0
+        for comment in all_comments:
+            worksheet.write(row, col, comment.rating, sheet_format)
+            worksheet.write(row, col + 1, comment.name, sheet_format)
+            worksheet.write(row, col + 2, comment.title + "\n" + comment.body, sheet_format)
+            date = comment.date.strftime('%Y-%m-%d %H:%M:%S')
+            worksheet.write(row, col + 3, date, sheet_format)
+            worksheet.write(row, col + 4, comment.country, sheet_format)
+            row += 1
 
-            #     --- load 2 for version
-
-            sleep_time = random.randint(1, 3)
-            time.sleep(sleep_time)
-
-    all_comments.sort(key=lambda cm: cm.date, reverse=True)
-
-    row = 1
-    col = 0
-    for comment in all_comments:
-        worksheet.write(row, col, comment.rating, sheet_format)
-        worksheet.write(row, col + 1, comment.name, sheet_format)
-        worksheet.write(row, col + 2, comment.title + "\n" + comment.body, sheet_format)
-        date = comment.date.strftime('%Y-%m-%d %H:%M:%S')
-        worksheet.write(row, col + 3, date, sheet_format)
-        worksheet.write(row, col + 4, comment.country, sheet_format)
-        row += 1
-
-    print("[%s_get_comments]finish! total: %s " % (app, count))
-    workbook.close()
+        print("[%s_get_comments]finish! total: %s " % (app_file, count))
+        workbook.close()
 
 
 if __name__ == '__main__':
-    os.chdir(os.path.abspath(os.path.dirname(sys.argv[0])))
+    path = os.path.abspath(os.path.join(os.path.dirname(sys.argv[0]), os.path.pardir))
+    os.chdir(path)
+    # print(path)
     # print(os.getcwd())
+    # print(sys.path)
+
+    sys.path.append(path)
+
+    app = 1
+    if len(sys.argv) >= 2:
+        print(sys.argv)
+        app = 2 if sys.argv[1] == '2' else 1
+
+    mark = 'SST' if app == 1 else 'Horse'
+    file = 'sst' if app == 1 else 'horse'
+
+    # print('%s %s %s' % (app, mark, file))
 
     load_dict = {}
-    # sst
 
-    print("[SST] begin ...")
-    with open('config_sst.json', 'r') as load_f:
+    print("[%s] begin ..." % mark)
+    with open('./comments/config.json', 'r') as load_f:
         load_dict = json.load(load_f)
 
-    print("[SST] %s", load_dict)
+    print("[%s] %s" % (mark, load_dict))
 
-    get_comments(load_dict['app_id'], load_dict['country'], 'sst')
-    # horse
-
-    print("[Horse] begin ...")
-    with open('config_horse.json', 'r') as load_f:
-        load_dict = json.load(load_f)
-
-    print("[Horse] %s", load_dict)
-
-    get_comments(load_dict['app_id'], load_dict['country'], 'horse')
+    com = GetComments()
+    com.get_comments(load_dict['app_id'], load_dict['country'], file)
 
     print("all done!")
-
